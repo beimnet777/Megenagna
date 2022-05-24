8c46cb4d9721aa216f144ca8a0fcf1cdda030584
Paulos Dessie Nedaw <pauldessie@gmail.com>
2022-05-24 11:53:39 +0300

    Added Company Profile to UI folder

--- Project-Megenagna/megenagna/lib/main.dart
+++ Project-Megenagna/megenagna/lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:megenagna/companyProfile.dart';
 import 'package:megenagna/ui/Home.dart';
 import 'package:megenagna/ui/Profile.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Home(),
+    home: CompanyProfile(),
     debugShowCheckedModeBanner: false,
     theme: ThemeData(
       fontFamily: 'Inter',
