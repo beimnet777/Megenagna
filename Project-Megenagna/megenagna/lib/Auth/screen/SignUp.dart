381ff25ff60ba827db2af118af04c70534b881af
Ted_Blackson <62517419+tewodros-web@users.noreply.github.com>
2022-06-02 11:23:09 +0300

    Update SignUp.dart

--- Project-Megenagna/megenagna/lib/Auth/screen/SignUp.dart
+++ Project-Megenagna/megenagna/lib/Auth/screen/SignUp.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:project/Auth/bloc/auth_bloc.dart';
 
-class Signup extends StatelessWidget {
-  Signup({Key? key}) : super(key: key);
+class SignupScreen extends StatelessWidget {
+  SignupScreen({Key? key}) : super(key: key);
 
   final OutlineInputBorder border = const OutlineInputBorder(
       borderSide:
@@ -12,6 +14,13 @@ class Signup extends StatelessWidget {
     var size = MediaQuery.of(context).size;
     RegExp validEmail =
         RegExp(r'([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})');
+
+    TextEditingController firstNamectrl = TextEditingController();
+    TextEditingController lastNamectrl = TextEditingController();
+    TextEditingController emailCtrl = TextEditingController();
+    TextEditingController addressCtrl = TextEditingController();
+    TextEditingController passwordCtrl = TextEditingController();
+    TextEditingController confirmPassCtrl = TextEditingController();
     return Scaffold(
         body: Form(
       key: _SignUpKey,
@@ -81,10 +90,8 @@ class Signup extends StatelessWidget {
             ),
             TextFormField(
               validator: (value) {
-                if(value == null || value.isEmpty){
-
+                if (value == null || value.isEmpty) {
                   return 'Please Enter your Address';
-
                 }
               },
               decoration: InputDecoration(
@@ -95,14 +102,70 @@ class Signup extends StatelessWidget {
             const SizedBox(
               height: 10,
             ),
-            ElevatedButton(
-                onPressed: () => {
-                  if (_SignUpKey.currentState!.validate()){
-                    ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(content: Text('Processing Data')))
-                  }
-                },
-                child: Text('Sign Up'))
+            TextFormField(
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return "Enter a Password";
+                } else if (value.length < 8) {
+                  return "password is too short ";
+                }
+              },
+              controller: passwordCtrl,
+              obscureText: true,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(
+                    borderSide: BorderSide(
+                        color: Colors.blue,
+                        width: 1,
+                        style: BorderStyle.solid)),
+                hintText: "Enter Password",
+              ),
+            ),
+            const SizedBox(height: 10),
+            TextFormField(
+              validator:(value) {
+                if (value != passwordCtrl.text){
+                  return 'Passwords are not same';
+                }
+              },
+              controller: confirmPassCtrl,
+              obscureText: true,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(
+                    borderSide: BorderSide(
+                        color: Colors.blue,
+                        width: 1,
+                        style: BorderStyle.solid)),
+                hintText: "Confirm Password",
+              ),
+            ),
+            const SizedBox(height: 10),
+            BlocConsumer<AuthBloc, AuthState>(
+                listener: (context, state) => Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => Scaffold(
+                            body: Center(child: Text('signed Up whoooooo'))))),
+                builder: (context, state) {
+                  final String firstName = firstNamectrl.text;
+                  final String lastName = lastNamectrl.text;
+                  final String email = emailCtrl.text;
+                  final String address = addressCtrl.text;
+                  final String password = passwordCtrl.text;
+                  return ElevatedButton(
+                      onPressed: () => {
+                            if (_SignUpKey.currentState!.validate())
+                              {
+                                BlocProvider.of<AuthBloc>(context).add(Register(
+                                    firstName: firstName,
+                                    lastName: lastName,
+                                    email: email,
+                                    address: address,
+                                    password: password))
+                              }
+                          },
+                      child: const Text('Register'));
+                })
           ],
         ),
       ),
