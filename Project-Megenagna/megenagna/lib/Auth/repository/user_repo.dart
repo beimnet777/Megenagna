dbd05a4e41457e17b42cad122cf9c4e7d5882b87
BEIMNET BEKELE GUTA <91521577+beimnet777@users.noreply.github.com>
2022-06-02 10:57:43 +0300

    Update user_repo.dart

--- Project-Megenagna/megenagna/lib/Auth/repository/user_repo.dart
+++ Project-Megenagna/megenagna/lib/Auth/repository/user_repo.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:megenagna/Auth/data%20provider/user_provider.dart';
 import 'package:megenagna/Auth/models/user.dart';
 
@@ -7,5 +9,18 @@ class UserRepository {
 
   Future<User> create(User user) async {
     return this.userProvider.createUser(user);
+  }
+
+   Future<User> get(User user) async {
+    return this.userProvider.getUser("${user.id}");
+  }
+
+  Future<dynamic> update(User user) async {
+
+    return this.userProvider.updateUser(user);
+  }
+
+  Future<dynamic> delete(User user) async {
+    return this.userProvider.deleteUser(user);
   }
 }
