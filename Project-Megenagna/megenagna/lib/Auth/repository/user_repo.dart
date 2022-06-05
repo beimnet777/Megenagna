82a8e4b97668856838868d33cf503bf1471e68c4
BEIMNET BEKELE GUTA <91521577+beimnet777@users.noreply.github.com>
2022-06-05 19:38:39 +0300

    Add files via upload

--- Project-Megenagna/megenagna/lib/Auth/repository/user_repo.dart
+++ Project-Megenagna/megenagna/lib/Auth/repository/user_repo.dart
@@ -1,26 +1,69 @@
-import 'dart:ffi';
+import '../data_provider/user_provider.dart';
+import '../models/user.dart';
 
-import 'package:megenagna/Auth/data%20provider/user_provider.dart';
-import 'package:megenagna/Auth/models/user.dart';
+import '../bloc/auth_bloc.dart';
 
 class UserRepository {
   final UserProvider userProvider;
   UserRepository(this.userProvider);
 
-  Future<User> create(User user) async {
+  Future create(Register register) async {
+    final int id = 0;
+    final String username = register.username;
+    final String password = register.password;
+    final String first_name = register.firstName;
+    final String last_name = register.lastName;
+    final String email = register.email;
+    int type = register.type;
+    User user = User(1, first_name, last_name, email, username, password, type);
+
     return this.userProvider.createUser(user);
   }
 
-   Future<User> get(User user) async {
-    return this.userProvider.getUser("${user.id}");
+  Future<User> get(User user) async {
+    return this.userProvider.getUser("");
   }
 
   Future<dynamic> update(User user) async {
-
     return this.userProvider.updateUser(user);
   }
 
   Future<dynamic> delete(User user) async {
     return this.userProvider.deleteUser(user);
   }
+
+  Future login(Login login) async {
+    return this.userProvider.loging(login);
+  }
+
+  Future getUserInfo() async {
+    return this.userProvider.getUserInfo();
+  }
 }
+
+
+// class UserDataRepository {
+//   final UserDataProvider userDataProvider;
+//   UserDataRepository(this.userDataProvider);
+
+//   Future create(Register register) async {
+//     final String username = register.username;
+//     final String password = register.password;
+//     int type;
+//     UserData userdata = UserData(username, password);
+
+//     return this.userDataProvider.createUserData(userdata);
+//   }
+
+//   Future<User> get(User user) async {
+//     return this.userDataProvider.getUserData("");
+//   }
+
+//   Future<dynamic> update(User user) async {
+//     return this.userProvider.updateUser(user);
+//   }
+
+//   Future<dynamic> delete(User user) async {
+//     return this.userProvider.deleteUser(user);
+//   }
+// }
