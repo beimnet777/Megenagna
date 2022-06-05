634ad3d966300923c23665e335e60734eeff3da9
Paulos Dessie Nedaw <93246229+PaulosNed@users.noreply.github.com>
2022-06-05 20:04:38 +0300

    Add files via upload

--- Project-Megenagna/Megenagna_back_end/megenagna_BACKEND/general_api/urls.py
+++ Project-Megenagna/Megenagna_back_end/megenagna_BACKEND/general_api/urls.py
@@ -1,12 +1,19 @@
-from django.urls import path,include
-from .views import EmployeeProfileView,EmployerProfileView,JobViewSet
+from django.urls import path, include
+from .views import EmployeeProfileView, EmployerProfileView, get_user, JobViewSet, EmployeeUserViewSet, EmployerUserViewSet, JobLister, ApplicationViewSet, get_application, get_jobs_company
 from rest_framework.routers import DefaultRouter
 
 router=DefaultRouter()
 router.register('jobs',JobViewSet,basename='job')
 router.register('employer',EmployerProfileView,basename='employer')
 router.register('employee',EmployeeProfileView,basename='employee')
+router.register('EmployeeUser',EmployeeUserViewSet,basename='EmplyeeUser')
+router.register('EmployerUser',EmployerUserViewSet,basename='EmployerUser')
+router.register('Application',ApplicationViewSet,basename='Application')
 
 urlpatterns = [ 
-          path('api/v1/model/',include(router.urls))
+          path('api/v1/model/',include(router.urls)),
+          path('api/v1/model/limited/',JobLister.as_view()),
+            path('jobs/<int:id>', get_jobs_company),
+            path('apps/<int:id>', get_application),
+            path('userInfo/', get_user)
 ]
