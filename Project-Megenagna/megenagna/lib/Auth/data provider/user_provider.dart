acf39f011c884721a9af13f9915c48c32ed4e2fb
BEIMNET BEKELE GUTA <91521577+beimnet777@users.noreply.github.com>
2022-06-05 19:36:34 +0300

    Add files via upload

--- Project-Megenagna/megenagna/lib/Auth/data provider/user_provider.dart
+++ Project-Megenagna/megenagna/lib/Auth/data provider/user_provider.dart
@@ -1,25 +1,39 @@
 import 'dart:convert';
+import 'package:last/Auth/bloc/auth_bloc.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-import 'package:megenagna/Auth/models/user.dart';
+import '/Auth/models/user.dart';
 import 'package:http/http.dart' as http;
 
-final String baseUrlEmployee = 'localhost:8000/api/v1/model/EmployeeUser';
-final String baseUrlEmployer = 'localhost:8000/api/v1/model/EmployerUser';
+final String baseUrlEmployee =
+    'http://127.0.0.1:8000/api/v1/model/EmployeeUser/';
+final String baseUrlEmployer =
+    'http://127.0.0.1:8000/api/v1/model/EmployerUser/';
+final String baseUrlEmployeeLogin = 'http://127.0.0.1:8000/auth/api/token/';
+final String baseUrlGetUser = 'http://127.0.0.1:8000/userInfo/';
 
 class UserProvider {
-  Future<User> createUser(User user) async {
-    if (user.type == 0) {
+  Future createUser(User user) async {
+    Map data = {
+      "first_name": user.username,
+      "last_name": user.password,
+      "email": user.first_name,
+      "username": user.last_name,
+      "password": user.email
+    };
+    if (user.type == 1) {
+      var body = json.encode(data);
       final response = await http.post(
         Uri.parse(baseUrlEmployee),
         headers: <String, String>{
           'Content-Type': 'application/json; charset=UTF-8',
         },
-        body: jsonEncode(user.jsonify()),
+        body: jsonEncode(data),
       );
-      if (response.statusCode == 200) {
+      if (response.statusCode == 201) {
         return User.fromJson(jsonDecode(response.body));
       } else {
-        throw Exception('Failed to load course');
+        return 0;
       }
     } else {
       final response = await http.post(
@@ -27,21 +41,41 @@ class UserProvider {
         headers: <String, String>{
           'Content-Type': 'application/json; charset=UTF-8',
         },
-        body: jsonEncode(user.jsonify()),
+        body: json.encode(data),
       );
-      if (response.statusCode == 200) {
+      if (response.statusCode == 201) {
         return User.fromJson(jsonDecode(response.body));
       } else {
-        throw Exception('Failed to load course');
+        
+        return 0;
       }
     }
   }
 
   Future<User> getUser(String id) async {
-    final response = await http.get(Uri.parse('$baseUrlEmployee/$id'));
+    final response = await http.get(
+      Uri.parse('$baseUrlEmployee/$id'),
+    );
 
     if (response.statusCode == 200) {
       return User.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to load course');
+    }
+  }
+
+  Future getUserInfo() async {
+    final prefs = await SharedPreferences.getInstance();
+    final String? token = prefs.getString('token');
+    final response =
+        await http.get(Uri.parse('$baseUrlGetUser'), headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+      'Authorization': 'Bearer $token'
+    });
+   
+
+    if (response.statusCode == 200) {
+      return (jsonDecode(response.body));
     } else {
       throw Exception('Failed to load course');
     }
@@ -80,7 +114,7 @@ class UserProvider {
         headers: <String, String>{
           'Content-Type': 'application/json; charset=UTF-8',
         },
-        body: jsonEncode(user.jsonify()),
+        body: jsonEncode(user.toJson()),
       );
       if (response.statusCode != 204) {
         throw Exception('Failed to load course');
@@ -91,11 +125,33 @@ class UserProvider {
         headers: <String, String>{
           'Content-Type': 'application/json; charset=UTF-8',
         },
-        body: jsonEncode(user.jsonify()),
+        body: jsonEncode(user.toJson()),
       );
       if (response.statusCode != 204) {
         throw Exception('Failed to load course');
       }
+    }
+  }
+
+  Future loging(Login login) async {
+    final http.Response response = await http.post(
+        Uri.parse(baseUrlEmployeeLogin),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: jsonEncode(<String, String>{
+          "username": login.username.toString(),
+          "password": login.password.toString()
+        }));
+
+    if (response.statusCode == 200) {
+      
+
+      return (jsonDecode(response.body));
+    } else {
+      // print(jsonDecode(response.body));
+
+      return 0;
     }
   }
 }
